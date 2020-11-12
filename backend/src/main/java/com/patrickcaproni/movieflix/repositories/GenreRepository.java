5be8fa2d04d7d70abedb83359f0efe557912e520
PatrickCaproni <patrick.-m@hotmail.com>
2020-11-12 14:31:57 -0300

    Repositories

---
+++ backend/src/main/java/com/patrickcaproni/movieflix/repositories/GenreRepository.java
@@ -0,0 +1,11 @@
+package com.patrickcaproni.movieflix.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.patrickcaproni.movieflix.entities.Genre;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, Long>{
+
+}
